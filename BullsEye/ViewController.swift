eff8a8a948a113f06131f7616bb4447457218a61
nicolehli <nicolehcli@gmail.com>
2018-08-13 19:23:22 -0700

    Refactor new round, add difference

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -13,17 +13,23 @@ class ViewController: UIViewController {
     var guess: Int = 0
     @IBOutlet weak var slider: UISlider!
     var targetValue: Int = 0
+    @IBOutlet weak var targetLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         guess = lroundf(slider.value)
-        targetValue = Int(arc4random_uniform(100)) + 1 // random 0-99 generated for our slider on 1-100
+        startNewRound()
     }
     
     func startNewRound() {
         targetValue = Int(arc4random_uniform(100)) + 1 // random 0-99 generated for our slider on 1-100
         guess = 50
         slider.value = Float(guess)
+        updateLabel()
+    }
+    
+    func updateLabel() {
+        targetLabel.text = String(targetValue)
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,8 +38,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
+        let difference: Int = abs(guess - targetValue)
         
-        let msg = "The value of the slider is: \(guess) \n The target value is \(targetValue)"
+        let msg = "The value of the slider is: \(guess)" +
+        "\nThe target value is \(targetValue)" +
+        "\nThe difference is \(difference)"
         
         let alert = UIAlertController(title: "Hello, World!", message: msg, preferredStyle: .alert)
         
@@ -42,6 +51,8 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
